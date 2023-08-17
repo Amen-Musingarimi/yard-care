80457c346a21ec6661b2faf97a07aecc10fc6eb1
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-08-17 12:25:17 +0200

    Add the About Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './components/styles/main.css';
 import NavBar from './components/NavBar';
 import Home from './components/pages/Home';
+import About from './components/pages/About';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" index element={<Home />} />
+        <Route path="/about" index element={<About />} />
       </Routes>
     </div>
   );
