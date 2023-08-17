875494b7b2754c1b58e6cdff4ce35f0f228b5ffd
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-08-17 19:48:58 +0200

    Add Step one styles

--- src/components/pages/ServiceForms/SteopOne.jsx
+++ src/components/pages/ServiceForms/SteopOne.jsx
@@ -1,34 +1,57 @@
 import React from 'react';
+import { useState } from 'react';
+import formImage from '../../images/form 1.webp';
+import '../../styles/Forms.css';
 
 const StepOne = ({ formData, nextStep, handleChange }) => {
   const { address, service } = formData;
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleNext = () => {
-    // Perform validation if needed before proceeding to the next step
+    if (!address || !service) {
+      setErrorMessage(
+        'Please fill in both the address and service fields before proceeding.'
+      );
+      return;
+    }
     nextStep();
   };
 
   return (
-    <div>
-      <h2>Step 1: Enter Address and Select Service</h2>
-      <input
-        type="text"
-        value={address}
-        onChange={(e) => handleChange('address', e.target.value)}
-        placeholder="Enter your address"
-      />
-      <select
-        value={service}
-        onChange={(e) => handleChange('service', e.target.value)}
-      >
-        <option value="">Select a service</option>
-        <option value="Lawn Mowing and Maintenance">
-          Lawn Mowing and Maintenance
-        </option>
-        <option value="Leaf and Debris Removal">Leaf and Debris Removal</option>
-        <option value="Garden Care">Garden Care</option>
-      </select>
-      <button onClick={handleNext}>Next</button>
+    <div className="step-div">
+      <div className="form-container">
+        <p className="step-description">
+          Kindly complete the brief form below, so that we can provide you with
+          an accurate quotation.
+        </p>
+        <div className="dotted-border-container"></div>
+        <span className="step-heading">Step 1 of 4</span>
+        <input
+          type="text"
+          value={address}
+          onChange={(e) => handleChange('address', e.target.value)}
+          placeholder="Enter your address"
+          required
+        />
+        <select
+          value={service}
+          onChange={(e) => handleChange('service', e.target.value)}
+          required
+        >
+          <option value="">Select a service</option>
+          <option value="Lawn Mowing and Maintenance">
+            Lawn Mowing and Maintenance
+          </option>
+          <option value="Leaf and Debris Removal">
+            Leaf and Debris Removal
+          </option>
+          <option value="Garden Care">Garden Care</option>
+        </select>
+        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+        <button onClick={handleNext}>Proceed</button>
+      </div>
+
+      <img src={formImage} alt="FormImage" />
     </div>
   );
 };
