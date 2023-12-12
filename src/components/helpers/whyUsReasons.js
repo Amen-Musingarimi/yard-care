1382673f240b3dd4a91f4d3bee762858c2fc4097
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-12-12 14:00:33 +0200

    Add relative icons

--- src/components/helpers/whyUsReasons.js
+++ src/components/helpers/whyUsReasons.js
@@ -1,41 +1,46 @@
-import leafIcon from '../images/icons/leaf-icon.svg';
+import userIcon from '../images/icons/user.svg';
+import clockIcon from '../images/icons/clock-icon.svg';
+import lawnMowerIcon from '../images/icons/lawnmower.svg';
+import heartIcon from '../images/icons/heart-icon.svg';
+import chattingIcon from '../images/icons/chatting.svg';
+import tapmeasure from '../images/icons/tape-measure.svg';
 
 const whyUsReasons = [
   {
     heading: 'Expertise You Can Trust',
     text:
       "Our team of seasoned professionals brings a wealth of expertise to your doorstep. With years of experience, we've perfected the art of transforming ordinary yards into extraordinary retreats.",
-    icon: leafIcon,
+    icon: userIcon,
   },
   {
-    heading: 'Tailored Solutions for Every Lawn',
+    heading: 'Tailored Solutions for Every Problem',
     text:
       "Just as every blade of grass is unique, so is each yard. YardCare Pro crafts personalized solutions to suit the specific needs of your green oasis. Whether it's lush lawns, vibrant flower beds, or meticulous tree care, we've got you covered.",
-    icon: leafIcon,
+    icon: tapmeasure,
   },
   {
     heading: 'Timely and Reliable Service',
     text:
       "We understand the importance of a well-maintained yard. That's why YardCare Pro prides itself on prompt and reliable service. Your satisfaction is our priority, and we ensure your outdoor space looks impeccable, season after season.",
-    icon: leafIcon,
+    icon: clockIcon,
   },
   {
     heading: 'Cutting-Edge Technology for Optimal Results',
     text:
       'Stay ahead in yard care innovation with YardCare Pro. We leverage cutting-edge technology to enhance our services, ensuring your lawn receives the best care possible. From precision mowing to eco-friendly pest control, we employ the latest tools to exceed your expectations.',
-    icon: leafIcon,
+    icon: lawnMowerIcon,
   },
   {
     heading: 'Customer-Centric Approach',
     text:
       'Your convenience matters to us. Our customer-centric approach means we work around your schedule. Need a one-time service or a recurring maintenance plan? YardCare Pro adapts to your needs with flexibility and efficiency.',
-    icon: leafIcon,
+    icon: heartIcon,
   },
   {
     heading: 'Transparent Communication',
     text:
       "We believe in transparent communication. At YardCare Pro, you'll always be in the loop. Receive updates, recommendations, and insights on your yard care journey, fostering a partnership built on trust.",
-    icon: leafIcon,
+    icon: chattingIcon,
   },
 ];
 
