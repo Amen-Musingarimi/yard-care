280c2bc65f176524d536d7a9150aaf316f828b25
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-12-12 11:24:34 +0200

    Add Whyus section structure

---
+++ src/components/pages/HomeSections/WhyUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../styles/WhyUs.css';
+
+const WhyUs = () => {
+  return (
+    <div className="whyus-container">
+      <h2 className="section-heading">
+        Transform Your Outdoor Space with YardCare
+      </h2>
+    </div>
+  );
+};
+
+export default WhyUs;
