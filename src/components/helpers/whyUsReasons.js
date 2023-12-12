00b74d067b2a702300bd3caba6beff67ad20c304
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-12-12 12:26:00 +0200

    Add why us reasons data

---
+++ src/components/helpers/whyUsReasons.js
@@ -0,0 +1,42 @@
+import leafIcon from '../images/icons/leaf-icon.svg';
+
+const whyUsReasons = [
+  {
+    heading: 'Expertise You Can Trust',
+    text:
+      "Our team of seasoned professionals brings a wealth of expertise to your doorstep. With years of experience, we've perfected the art of transforming ordinary yards into extraordinary retreats.",
+    icon: leafIcon,
+  },
+  {
+    heading: 'Tailored Solutions for Every Lawn',
+    text:
+      "Just as every blade of grass is unique, so is each yard. YardCare Pro crafts personalized solutions to suit the specific needs of your green oasis. Whether it's lush lawns, vibrant flower beds, or meticulous tree care, we've got you covered.",
+    icon: leafIcon,
+  },
+  {
+    heading: 'Timely and Reliable Service',
+    text:
+      "We understand the importance of a well-maintained yard. That's why YardCare Pro prides itself on prompt and reliable service. Your satisfaction is our priority, and we ensure your outdoor space looks impeccable, season after season.",
+    icon: leafIcon,
+  },
+  {
+    heading: 'Cutting-Edge Technology for Optimal Results',
+    text:
+      'Stay ahead in yard care innovation with YardCare Pro. We leverage cutting-edge technology to enhance our services, ensuring your lawn receives the best care possible. From precision mowing to eco-friendly pest control, we employ the latest tools to exceed your expectations.',
+    icon: leafIcon,
+  },
+  {
+    heading: 'Customer-Centric Approach',
+    text:
+      'Your convenience matters to us. Our customer-centric approach means we work around your schedule. Need a one-time service or a recurring maintenance plan? YardCare Pro adapts to your needs with flexibility and efficiency.',
+    icon: leafIcon,
+  },
+  {
+    heading: 'Transparent Communication',
+    text:
+      "We believe in transparent communication. At YardCare Pro, you'll always be in the loop. Receive updates, recommendations, and insights on your yard care journey, fostering a partnership built on trust.",
+    icon: leafIcon,
+  },
+];
+
+export default whyUsReasons;
--- src/components/pages/HomeSections/WhyUs.jsx
+++ src/components/pages/HomeSections/WhyUs.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import whyUsReasons from '../../helpers/whyUsReasons';
 import '../../styles/WhyUs.css';
 
 const WhyUs = () => {
   return (
     <div className="whyus-container">
-      <h2 className="section-heading">
-        Transform Your Outdoor Space with YardCare
-      </h2>
+      <div className="heading-div">
+        <h2 className="section-heading">
+          Transform Your Outdoor Space with YardCare
+        </h2>
+        <p className="supporting-text">
+          Embrace the beauty of your outdoor haven with YardCare Pro, where
+          exceptional service meets pristine landscapes. Discover why we stand
+          out in the yard care industry:
+        </p>
+      </div>
+      <div className="reasons-container">
+        {whyUsReasons.map((reason, index) => (
+          <div key={index} className="reason">
+            <div className="icon">{reason.icon}</div>
+            <div className="reason-content">
+              <h3 className="reason-heading">{reason.heading}</h3>
+              <p className="reason-text">{reason.text}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
