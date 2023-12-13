07651a04ecd2df749b8b71f7253c600aeb7a2451
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-12-13 11:59:23 +0200

    Style the testimonials

--- src/components/pages/HomeSections/Testimonials.jsx
+++ src/components/pages/HomeSections/Testimonials.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import customerTestimonials from '../../helpers/testimonialData';
 import '../../styles/Testimonials.css';
-import { GrNext, GrPrevious } from 'react-icons/gr';
-import { FaQuoteLeft } from 'react-icons/fa';
+import { ImQuotesLeft } from 'react-icons/im';
+import { ImQuotesRight } from 'react-icons/im';
 
 const Testimonials = () => {
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
@@ -19,44 +19,32 @@ const Testimonials = () => {
     };
   }, []);
 
-  const previousCard = () => {
-    setCurrentCardIndex(
-      (prevIndex) =>
-        (prevIndex - 1 + customerTestimonials.length) %
-        customerTestimonials.length
-    );
-  };
-
-  const nextCard = () => {
-    setCurrentCardIndex(
-      (prevIndex) => (prevIndex + 1) % customerTestimonials.length
-    );
-  };
-
   const currentCard = customerTestimonials[currentCardIndex];
 
   return (
     <div className="testimonialsContainer">
-      <h2 className="sectionHeading">What People Think About Us</h2>
-      <div className="borderTop"></div>
+      <div className="heading-cont">
+        <h2 className="sectionHeading">We Give Awesome Service,</h2>
+        <h2 className="sectionHeading">
+          See How <span className="dif-text">Customers Love Us.</span>
+        </h2>
+      </div>
+
       <div className="quoteContainer">
-        <button className="quoteBtn" onClick={previousCard}>
-          <GrPrevious className="icon" />
-        </button>
-        <div className="quoteCard">
-          <img src={currentCard.image} alt="UserImage" className="userImager" />
-          <span className="quoteSign">
-            <FaQuoteLeft />
-          </span>
+        <div className="quote-cont">
+          <ImQuotesLeft />
           <p className="quote">{currentCard.testimonial}</p>
-          <div className="nameCont">
-            <h3 className="clientName">{currentCard.name}</h3>
-            <p className="clientProfession">{currentCard.location}</p>
-          </div>
+          <ImQuotesRight />
         </div>
-        <button className="quoteBtn" onClick={nextCard}>
-          <GrNext className="icon" />
-        </button>
+        <div className="customer-details">
+          <img
+            src={currentCard.image}
+            alt="Customer"
+            className="customer-image"
+          />
+          <h4 className="customer-name">{currentCard.name}</h4>
+          <p className="customer-location">{currentCard.location}</p>
+        </div>
       </div>
     </div>
   );
