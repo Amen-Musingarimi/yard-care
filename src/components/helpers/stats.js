24608db753b0a82e48a71848fa2599e363dd713c
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-12-13 08:54:33 +0200

    Add Yard Care stats data

---
+++ src/components/helpers/stats.js
@@ -0,0 +1,29 @@
+import users from '../images/icons/users.svg';
+import map from '../images/icons/map.svg';
+import heart from '../images/icons/heart.svg';
+import filter from '../images/icons/filter.svg';
+
+const yardCareStats = [
+  {
+    icon: users,
+    number: '3654+',
+    text: 'Regular Customers',
+  },
+  {
+    icon: map,
+    number: '300+',
+    text: 'Areas Covered',
+  },
+  {
+    icon: heart,
+    number: '2500+',
+    text: 'Loved By',
+  },
+  {
+    icon: filter,
+    number: '07+',
+    text: 'Customizable Services',
+  },
+];
+
+export default yardCareStats;
