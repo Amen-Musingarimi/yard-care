41d42540a2b5cf154a6501185c74bf58a70e2c23
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-12-13 12:10:13 +0200

    Fix the customers images

--- src/components/helpers/testimonialData.js
+++ src/components/helpers/testimonialData.js
@@ -1,8 +1,8 @@
 import lady1 from '../images/customers/lady1.jpg';
 import man1 from '../images/customers/man1.jpg';
 import lady2 from '../images/customers/lady2.jpg';
-import man2 from '../images/customers/man2.jpg';
 import man3 from '../images/customers/man3.jpg';
+import manMe from '../images/customers/myself.jpg';
 import lady3 from '../images/customers/lady3.jpg';
 
 const customerTestimonials = [
@@ -10,42 +10,42 @@ const customerTestimonials = [
     name: 'Jane Smith',
     location: 'Harare, Zimbabwe',
     testimonial:
-      'YardCare Pro transformed my lawn into a green paradise. Their lawn mowing and maintenance services are top-notch. I highly recommend them for anyone in need of professional yard care.',
+      'YardCare transformed my lawn into a green paradise. Their lawn mowing and maintenance services are top-notch. I highly recommend them for anyone in need of professional yard care.',
     image: lady1,
   },
   {
     name: 'Voice Tongo',
     location: 'Borrowdale, Harare',
     testimonial:
-      "I was impressed with YardCare Pro's attention to detail in garden care. They meticulously tend to my garden, ensuring it looks beautiful throughout the year. Great service!",
+      "I was impressed with YardCare's attention to detail in garden care. They meticulously tend to my garden, ensuring it looks beautiful throughout the year. Great service!",
     image: lady2,
   },
   {
     name: 'Robert Maposa',
     location: 'Avondale, Harare',
     testimonial:
-      'Hardscape cleaning has never been this easy! YardCare Pro did an excellent job pressure washing my deck and maintaining my fence. They are reliable and efficient.',
+      'Hardscape cleaning has never been this easy! YardCare did an excellent job pressure washing my deck and maintaining my fence. They are reliable and efficient.',
     image: man1,
   },
   {
-    name: 'Grace Mutizwa',
-    location: 'Chisipite, Harare',
+    name: 'Amen Musinga',
+    location: 'Mabelreign, Harare',
     testimonial:
-      "YardCare Pro's tree services are unparalleled. They pruned and shaped my trees beautifully, enhancing the overall look of my property. I'm a satisfied customer!",
-    image: man2,
+      "YardCare's tree services are unparalleled. They pruned and shaped my trees beautifully, enhancing the overall look of my property. I'm a satisfied customer!",
+    image: manMe,
   },
   {
     name: 'Samuel Ncube',
     location: 'Greendale, Harare',
     testimonial:
-      'Irrigation system maintenance made easy with YardCare Pro. They keep my plants healthy and my system running efficiently. Professional and reliable service!',
+      'Irrigation system maintenance made easy with YardCare. They keep my plants healthy and my system running efficiently. Professional and reliable service!',
     image: man3,
   },
   {
     name: 'Alice Sibanda',
     location: 'Mount Pleasant, Harare',
     testimonial:
-      "I recently had landscaping enhancements done by YardCare Pro, and I couldn't be happier. My outdoor space has been transformed with beautiful features. Thank you!",
+      "I recently had landscaping enhancements done by YardCare, and I couldn't be happier. My outdoor space has been transformed with beautiful features. Thank you!",
     image: lady3,
   },
 ];
